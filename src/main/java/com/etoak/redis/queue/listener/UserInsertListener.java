6b428cc8eab1eca54cdb0fb1a74654967308978a
yang8364 <836460399@qq.com>
2017-07-13 16:39:42 +0800

    添加log

--- src/main/java/com/etoak/redis/queue/listener/UserInsertListener.java
+++ src/main/java/com/etoak/redis/queue/listener/UserInsertListener.java
@@ -2,6 +2,8 @@ package com.etoak.redis.queue.listener;
 
 import java.io.Serializable;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.etoak.pojo.User;
@@ -9,10 +11,13 @@ import com.etoak.service.UserService;
 
 public class UserInsertListener {
 	
+	private static Log log = LogFactory.getLog(UserInsertListener.class);
+	
 	@Autowired
 	private UserService userService;
 	
 	public void handleMessage(Serializable message) {
+		log.info("UserInsertListener start");
 		User user = (User) message;
 		System.out.println(user.getUserName());
 		Integer count =  userService.saveUser(user, "add");
